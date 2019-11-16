1b3139d29b40c7ff29e76ecec58c0b56469a28dd
21praveen <21praveen@users.noreply.github.com>
2019-11-17 03:43:05 +0800

    Update quetra.js
    Copyright added

--- code/quetra.js
+++ code/quetra.js
@@ -1,3 +1,5 @@
+//Copyright (c) 2018-2019 by Atlastream Pte. Ltd. All rights reserved.
+
 import SwitchRequest from '../SwitchRequest.js';
 import BufferController from '../../controllers/BufferController.js';
 import AbrController from '../../controllers/AbrController.js';
